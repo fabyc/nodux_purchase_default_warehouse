74c7512f9b80cce41becae61c6b8efd50037cc3d
Tatiana Quizhpe <tatianaq@nodux.ec>
2016-05-13 15:07:41 -0500

    Add error control

--- purchase.py
+++ purchase.py
@@ -13,15 +13,18 @@ class Purchase():
     'Purchase'
     __name__ = 'purchase.purchase'
     _rec_name = 'reference'
-    
+
     @classmethod
     def default_warehouse(cls):
         pool = Pool()
         Config = pool.get('purchase.configuration')
-        w = Config(1).warehouse
-        return w.id
-        
-        
+        if Config(1).warehouse:
+            w = Config(1).warehouse
+            return w.id
+        else:
+            cls.raise_user_error("No ha configurado la bodega por defecto, dirijase a: \n Configuracion-Configuracion de Compras")
+
+
     @classmethod
     def __setup__(cls):
         super(Purchase, cls).__setup__()
